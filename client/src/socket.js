76fda8f3ba38cf96476cff242ad57389f22151ff
Harshi264 <harshiharshi2604@gmail.com>
2025-08-05 22:32:02 +0530

    Update socket URL to point to Render backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // Get username from localStorage
 const username = localStorage.getItem("username");
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://drawbattle-realtime.onrender.com", {
   auth: { username },           // Send username to backend
   autoConnect: false,           // Connect manually in components
   transports: ["websocket"],    // Use WebSocket directly
